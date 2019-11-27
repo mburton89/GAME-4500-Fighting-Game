0b9df1922e69fa217b0772999516c8362f8e0fa7
Matt Burton <matthewwburton@gmail.com>
2019-11-27 17:08:06 -0600

    all teh things

---
+++ GAME 4500 Fighting Game/Assets/FighterSelectMenu/Scripts/BackgroundCircle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCircle : MonoBehaviour
+{
+    public float xRotation = 90.0f;
+    public float xRotation1 = 0.0f;
+    public float RotationSpeed = 2.0f;
+    private Vector3 clockwise = new Vector3(0,0,1);
+    void Update()
+    {
+        //transform.eulerAngles = new Vector3(xRotation, transform.eulerAngles.y, transform.eulerAngles.z);
+        transform.Rotate(clockwise * (RotationSpeed * Time.deltaTime));
+    }
+}
