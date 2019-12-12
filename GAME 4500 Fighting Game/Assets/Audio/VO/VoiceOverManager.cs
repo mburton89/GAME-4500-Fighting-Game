70edda24f30ee80dd2bfe20a4e3ed9d72fcbe79e
Matt Burton <matthewwburton@gmail.com>
2019-12-11 18:43:15 -0600

    initial commit

---
+++ GAME 4500 Fighting Game/Assets/Audio/VO/VoiceOverManager.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class VoiceOverManager : MonoBehaviour
+{
+    public List<AudioClip> confirmCharacterVO;
+}
